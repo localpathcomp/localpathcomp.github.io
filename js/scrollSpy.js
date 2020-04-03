85e19ad07737441262f8399a6ff637d2f39389e6
Garrick Crouch <services@localpathcomputing.com>
2020-04-02 23:05:59 -0500

    add transition on nav bg scroll spy

--- js/scrollSpy.js
+++ js/scrollSpy.js
@@ -23,6 +23,33 @@ const scrollSpy = () => {
     sections.forEach((el, i) => {
         intersectionObserver.observe(sections[i]);
     })
+    const navBar = document.getElementById('navBar');
+    const spot = document.querySelector('.spot');
+    const handleScroll = entries => {
+        const spotIsVisible = entries[0].isIntersecting;
+        if (spotIsVisible) {
+            navBar.style.background = "rgba(0,0,0,0)";
+            navBar.addEventListener('transitionend', () => {
+                navBar.classList.remove('scrolling');
+            }, {
+                capture: false,
+                once: true,
+                passive: false
+            });
+        } else {
+            navBar.style.background = "rgba(0,0,0,0.75)";
+            navBar.classList.add('scrolling');
+        }
+
+    };
+
+    const options = {
+        root: null,
+        rootMargin: "0px",
+        threshhold: 0
+    };
+    const observer = new IntersectionObserver(handleScroll, options);
+    observer.observe(spot);
 }
 
 export default scrollSpy;
